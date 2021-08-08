d64b82ac591622d57daa6e6cb357e7bb3b140c63
takurusu <taku.a.k.a.mizutani@gmail.com>
2021-08-08 05:39:01 +0000

    [Finish-Commit]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  
+  before_action :correct_user, only: [:edit, :update]
+  
   def new
     @book = Book.new
   end
@@ -50,5 +53,15 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body ,:image)
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+   
+  def correct_user
+    @book =Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :correct_user, only: [:edit, :update]
+  
   def index
     @user = User.find(current_user.id)
     @book = Book.new
@@ -29,5 +32,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(user_path(current_user)) unless @user == current_user
   end
 end
